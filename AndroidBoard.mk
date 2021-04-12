11ff29a84390f4ef5e00f047a49b022e01672aeb
Aaron Kling <webgeek1234@gmail.com>
2021-04-12 00:03:28 -0500

    Build main dtimages
    Change-Id: I937fd7f3142e6ff2ec415f0bfdcc353d3f914ddd

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -15,3 +15,24 @@
 #
 
 -include device/nvidia/foster/AndroidBoard.mk
+
+INSTALLED_DTBIMAGE_TARGET_mdarcy := $(PRODUCT_OUT)/install/mdarcy.dtb.img
+$(INSTALLED_DTBIMAGE_TARGET_mdarcy): $(INSTALLED_KERNEL_TARGET) | mkdtimg
+	echo -e ${CL_GRN}"Building mdarcy DTImage"${CL_RST}
+	@mkdir -p $(PRODUCT_OUT)/install
+	$(HOST_OUT_EXECUTABLES)/mkdtimg create $@ --id=2894 \
+		$(KERNEL_OUT)/arch/arm64/boot/dts/tegra210b01-darcy-p2894-0050-a08-00.dtb --rev=0x0a8 --custom0=0x28 \
+		$(KERNEL_OUT)/arch/arm64/boot/dts/tegra210b01-darcy-p2894-2551-b00-00.dtb --rev=0xb00 --custom0=2551 \
+		$(KERNEL_OUT)/arch/arm64/boot/dts/tegra210b01-darcy-p2894-3551-b03-00.dtb --rev=0xb03 --custom0=3551
+
+INSTALLED_DTBIMAGE_TARGET_sif    := $(PRODUCT_OUT)/install/sif.dtb.img
+$(INSTALLED_DTBIMAGE_TARGET_sif): $(INSTALLED_KERNEL_TARGET) | mkdtimg
+	echo -e ${CL_GRN}"Building sif DTImage"${CL_RST}
+	@mkdir -p $(PRODUCT_OUT)/install
+	$(HOST_OUT_EXECUTABLES)/mkdtimg create $@ --id=3425 --custom0=0x140 \
+		$(KERNEL_OUT)/arch/arm64/boot/dts/tegra210b01-sif-p3425-0500-a01.dtb --rev=0xa1 \
+		$(KERNEL_OUT)/arch/arm64/boot/dts/tegra210b01-sif-p3425-0500-a02.dtb --rev=0xa2 \
+		$(KERNEL_OUT)/arch/arm64/boot/dts/tegra210b01-sif-p3425-0500-a04.dtb --rev=0xa3 \
+		$(KERNEL_OUT)/arch/arm64/boot/dts/tegra210b01-sif-p3425-0500-a04.dtb --rev=0xa4
+
+ALL_DEFAULT_INSTALLED_MODULES += $(INSTALLED_DTBIMAGE_TARGET_mdarcy) $(INSTALLED_DTBIMAGE_TARGET_sif)
